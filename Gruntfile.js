193e8732863b246f7f13a363e15abb08b3bed3da
Mathias We <mathes111@gmail.com>
2016-07-18 19:06:53 +0200

    reformatting
    also moved init script to its own file
    and css to vendor subfolder

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,45 +1,44 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 
-    jshint: {
-      files: [
-        'Gruntfile.js',
-        'src/js/*.js'
-      ]
-    },
+		jshint: {
+			files: [
+				'Gruntfile.js',
+				'src/js/*.js'
+			]
+		},
 
-    watch: {
-      files: [
-      'src/main/js/*.js',
-      'src/main/**/*.html',
-      'src/main/css/*.css'
-      ],
-      options: {
-        livereload: true,
-      }
-    },
+		watch: {
+			files: [
+				'src/main/js/*.js',
+				'src/main/**/*.html',
+				'src/main/css/*.css'
+			],
+			options: {
+				livereload: true,
+			}
+		},
 
-    connect: {
-      server: {
-        options: {
-          base: '.',
-          port: 8082,
-          keepalive: true
-        }
-      }
-    }
-  });
+		connect: {
+			server: {
+				options: {
+					base: '.',
+					port: 8082,
+					keepalive: true
+				}
+			}
+		}
+	});
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', [
-    'jshint'
-  ]);
+	grunt.registerTask('default', [
+		'jshint'
+	]);
 
-
-  grunt.registerTask('server', ['connect']);
+	grunt.registerTask('server', ['connect']);
 };
---
+++ js/init.js
@@ -0,0 +1,34 @@
+(function ($) {
+	$(document).foundation();
+	if (!Modernizr.svg || !Modernizr.inlinesvg) {
+		$('#loadEverything').attr('disabled', true);
+		$('.not-supported').show();
+	} else {
+
+		$('#loadEverything').on('click', function () {
+			d3.json("data/sb2016.json", function (data) {
+				var content = d3.select('#main-content'),
+					nav = d3.select('#main-nav');
+
+				data.days.forEach(function (day, i) {
+					var dayName = day.day,
+						dayNameLowercase = dayName.toLowerCase();
+
+					nav.append('li')
+						.attr('class', 'tab-title active')
+						.append('a')
+						.attr('href', '#' + dayNameLowercase)
+						.text(dayName);
+
+					content.append('section')
+						.attr('class', 'content')
+						.attr('id', dayNameLowercase)
+						.datum(day)
+						.call(runningOrderVisualizer);
+				});
+			});
+
+			$('#loadEverything').hide();
+		});
+	}
+})(jQuery);
